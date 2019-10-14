58cb79d7387bad41e508773a0aa51e43c633215b
Pham Anh Dung <anhdungpham090@gmail.com>
2019-10-14 20:04:14 +0700

    Tao ham tao file va doc file.

--- NoteApp/MainPage.xaml.cs
+++ NoteApp/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Debug = System.Diagnostics.Debug;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,5 +30,42 @@ namespace NoteApp
         {
             this.InitializeComponent();
         }
+
+
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+            var name = CreateNoteFile(this.RebCustom.Text);
+            var content = ReadNoteFile(name);
+            Debug.WriteLine(content);
+        }
+        private static string CreateNoteFile(string input)
+        {
+            var name = DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss-tt") + ".txt";
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var noteFile = storageFolder.CreateFileAsync(name, CreationCollisionOption.ReplaceExisting).GetAwaiter().GetResult();
+            FileIO.WriteTextAsync(noteFile, input).GetAwaiter().GetResult();
+            return name;
+        }
+
+        private static string ReadNoteFile(string fileName)
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var sampleFile = storageFolder.GetFileAsync(fileName).GetAwaiter().GetResult();
+            Debug.WriteLine(sampleFile.Path);
+            var content = FileIO.ReadTextAsync(sampleFile).GetAwaiter().GetResult();
+            return content;
+        }
+
+        private async void GetFile()
+        {
+            var appInstalledFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var assets = await appInstalledFolder.GetFolderAsync("Assets");
+            var files = await assets.GetFilesAsync();
+            foreach (var file in files)
+            {
+                var content = ReadNoteFile(file.Name);
+            }
+        }
     }
+
 }
